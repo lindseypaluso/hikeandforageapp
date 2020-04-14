f50667f67b1e5f7524e06f34f8998095e2d13d6b
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-04-14 01:51:53 -0600

    lisa's site design (unifinished

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// mapbox://styles/lisamcamp/ck8zfacri00a81iqshqireqqe
+// https://api.mapbox.com/styles/v1/lisamcamp/ck8zfacri00a81iqshqireqqe.html?fresh=true&title=view&access_token=pk.eyJ1IjoibGlzYW1jYW1wIiwiYSI6ImNrOHpleHlzYTAxcWkzZnBlcjdxM3BoZnIifQ.bNRIw_e-uCQLRGfy9-bHlQ
+// mapboxgl.accessToken = pk.eyJ1IjoibGlzYW1jYW1wIiwiYSI6ImNrOHpleHlzYTAxcWkzZnBlcjdxM3BoZnIifQ.bNRIw_e-uCQLRGfy9-bHlQ;
+
+
+// Carousel image rotation function
+
+$(document).ready(function() {
+    $('.carousel').carousel('cycle') ({
+        interval: 2000
+      });
+});
