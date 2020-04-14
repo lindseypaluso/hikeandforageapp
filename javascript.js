870ff8976519e26674c8db96cdeaab2270167c15
Abigail Nielson <aa.nielson18@gmail.com>
2020-04-14 12:14:11 -0600

    added some comments with information about the all trails API

--- javascript.js
+++ javascript.js
@@ -10,3 +10,20 @@ $(document).ready(function() {
         interval: 2000
       });
 });
+
+//Hiking Project Data API
+//API key: 200727629-d773c339e8dcd5aa90cb10c2a18cde1f
+//Different methods available
+  // getTrails - returns trails for given query
+    //required arguments: key - Your private key, lat - Latitude for a given area, lon - Longitude for a given area
+    //optional arguments: maxDistance, maxResults, sort, minLength, minStars
+    //example syntax: https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200727629-d773c339e8dcd5aa90cb10c2a18cde1f
+  //getTrailsById
+    //required arguments: key & ids
+    //maybe we would want to use this if we're focusing on 10 specific hikes?
+    //example: https://www.hikingproject.com/data/get-trails-by-id?ids=7001635,7002742,7006663,7000108,7002175&key=200727629-d773c339e8dcd5aa90cb10c2a18cde1f
+  //getConditions
+    //we could maybe use this for crowdedness or prettiness or dificulty? not sure what available conditions there are
+    //required: ids
+    //example: https://www.hikingproject.com/data/get-conditions?ids=7001635,7002742,7006663,7000108,7002175&key=200727629-d773c339e8dcd5aa90cb10c2a18cde1f
+  
